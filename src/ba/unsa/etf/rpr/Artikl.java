99efe92043d551a1fb28ba40acd232b0a5c0b3d0
mbesirevic1 <mbesirevic1@etf.unsa.ba>
2020-10-30 17:42:56 +0100

    Zapocete klase Artikl i Korpa

--- src/ba/unsa/etf/rpr/Artikl.java
+++ src/ba/unsa/etf/rpr/Artikl.java
@@ -1,4 +1,26 @@
 package ba.unsa.etf.rpr;
 
 public class Artikl {
+
+    private String naziv;
+    private int cijena;
+    private String kod;
+
+
+
+    public Artikl(String naziv, int cijena, String kod){
+        this.naziv = naziv;
+        this.cijena = cijena;
+        this.kod = kod;
+    }
+
+    public final String dajNaziv(){
+        return naziv;
+    }
+    public final int dajCijenu(){
+        return cijena;
+    }
+    public final String dajKod(){
+        return kod;
+    }
 }
--- src/ba/unsa/etf/rpr/Korpa.java
+++ src/ba/unsa/etf/rpr/Korpa.java
@@ -1,4 +1,40 @@
 package ba.unsa.etf.rpr;
 
 public class Korpa {
+
+    private Artikl[] artikli;
+    private int broj_artikala_u_korpi;
+
+    Korpa(){
+        artikli = new Artikl[50];
+        broj_artikala_u_korpi = 0;
+    }
+    public Artikl izbaciArtkilSaKodom(String kod){
+
+        Artikl a = null;
+        for(int i=0; i<broj_artikala_u_korpi; i++){
+            if(artikli[i].dajKod().equals(kod)){
+                a = artikli[i];
+                artikli[i] = null;
+                broj_artikala_u_korpi--;
+                break;
+            }
+        }
+
+        return a;
+    }
+
+    public final int dajUkupnuCijenuArtikala(){
+        int cijena = 0;
+        for(int i=0; i<broj_artikala_u_korpi; i++){
+            cijena += artikli[i].dajCijenu();
+        }
+
+        return cijena;
+    }
+
+
+
+
+
 }
